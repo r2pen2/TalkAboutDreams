c7cf71019ea1d2cdc90309cb5fc5ff9fa973d275
r2pen2 <joedobbelaar@gmail.com>
2023-09-19 23:53:25 -0400

    tests fixed?

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -40,10 +40,10 @@ export function App(props) {
       { isTestingEnvironment && <meta data-testid="wl-testing-flag" /> }
       <Router>
         <div className="app-content">
-          <section className="d-flex flex-column align-items-center justify-content-center" style={{height: "100vh"}}>
-            <img src={powerBrick} alt="power-brick" data-testid="lego-brick" />
-            <Text h1 data-testid="title-text">BP-10700</Text>
-          </section>
+            <section className="d-flex flex-column align-items-center justify-content-center" style={{height: "100vh", width: "100vw"}}>
+              <img src={powerBrick} alt="power-brick" data-testid="lego-brick" />
+              <Text h1 data-testid="title-text">BP-10700</Text>
+            </section>
           {/** Place Navigation Here */}
             <Routes>
               {/** Place Routes Here */}
