9d5280c567c10e9d45e3014480fa81cb9c0657f7
r2pen2 <joedobbelaar@gmail.com>
2023-10-03 00:13:07 -0400

    made the bridge, nav, and button

--- client/src/routes/HomepageV2.jsx
+++ client/src/routes/HomepageV2.jsx
@@ -4,33 +4,40 @@ import {WLHeader, WLHeaderV2, WLLayoutHeader, WLLayoutText, WLTextV2} from "../l
 
 import bridge from "../assets/images/bridge.svg";
 import flower from "../assets/images/leaf.png";
-import { swanWhite } from '../libraries/Web-Legos/api/colors';
+import { swanBlueGray, swanWhite } from '../libraries/Web-Legos/api/colors';
+import { ButtonRounded } from '../libraries/Web-Legos/Layouts/Buttons/Buttons';
+import NavbarMinimal from '../libraries/Web-Legos/Layouts/Navigation/Navbars';
 
 export default function HomepageV2() {
-
-  const Bio = () => (
-    <article className="d-flex flex-column align-items-center justify-content-center">
-
-    </article>
-  )
-
-  const Goal = () => (
-    <article className="d-flex flex-column align-items-center justify-content-center">
-
-    </article>
-  )
-
-  const FlowerGlyph = () => <img src={flower} alt="flower" />
 
   return (
     <div className="d-flex flex-column align-items-center justify-content-center">
       <section className="splash d-flex flex-column align-items-center justify-content-center">
-        <hgroup style={{maxWidth: 650}}>
+        <NavbarMinimal sticky>
+          <NavbarMinimal.Item href="/#home">
+            Home
+          </NavbarMinimal.Item>
+          <NavbarMinimal.Item href="/#about">
+            Who I Am
+          </NavbarMinimal.Item>
+          <NavbarMinimal.Item href="/#services">
+            Services
+          </NavbarMinimal.Item>
+          <NavbarMinimal.Item href="/#contact">
+            Contact
+          </NavbarMinimal.Item>
+        </NavbarMinimal>
+        <hgroup style={{maxWidth: 700}}>
           <WLHeaderV2 align="center" color="white">Julia Dobbelaar</WLHeaderV2>
-          <WLTextV2 align="center" className="fw-sb" color="">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia vitae sequi eum, suscipit delectus expedita ad itaque nulla nam blanditiis velit, quasi deleniti quae fugit enim accusantium cumque totam nihil!
+          <WLTextV2 align="center" className="fw-sb" color={swanBlueGray}>
+            This is a place for you to talk about yourself as a dream analyst. This is a place for you to talk about yourself as a dream analyst. This is a place for you to talk about yourself as a dream analyst. This is a place for you to talk about yourself as a dream analyst. This is a place for you to talk about yourself as a dream analyst. This is a place for you to talk about yourself as a dream analyst.
           </WLTextV2>
         </hgroup>
+        <div className="wl-spacer-2" />
+        <ButtonRounded color="#5738B3" size="lg">
+          Speak With Me
+        </ButtonRounded>
+        <img src={bridge} alt="bridge" className="wl-section-bottom"/>
       </section>
       <section>
         <GlyphSectionTwoItemsNoActions
